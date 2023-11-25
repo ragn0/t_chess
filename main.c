074cc469b74d00c0df012eb2d19f9e949ac976cc
ragn0 <giordanoragnacci@gmail.com>
2023-11-25 19:02:03 +0100

    Secondo commit, implementato i colori nella funzione stampa scacchiera, definito la struttura Scacchiera per facilitare la funzione stampa_scacchiera e implementato la funzione ruota_scacchiera per ruotare la scacchiera alla fine del turno di uno dei due giocatori

--- main.c
+++ main.c
@@ -1,29 +1,64 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 
-void stampa_scacchiera(char **scacchiera)
+// Definisco la struttura scacchiera (NOTA: la struttura è comoda perchè posso dichiarare anche il parametro ruotata per
+// facilitare la funzione stampa_scacchiera e per future features che si potranno implementare)
+typedef struct Scacchiera
 {
-    printf("   a b c d e f g h\n");
+    char **scacchiera;
+    int ruotata;
+} Scacchiera;
+
+// Stampo la scacchiera data una scacchiera s, in base a se il turno è del bianco o del nero sarà ruotata a dovere
+void stampa_scacchiera(Scacchiera s)
+{
+    if (!(s.ruotata))
+        printf("   a b c d e f g h\n");
+    else
+        printf("   h g f e d c b a\n");
     printf("------------------\n");
-    for (int i = 0; i < 8; i++)
+    for (int i = 0, len = 8; i < 8; i++, len--)
     {
-        printf("%d| ", i + 1);
+        if (!(s.ruotata))
+            printf("%d| ", len);
+        else
+            printf("%d| ", i + 1);
+
         for (int j = 0; j < 8; j++)
         {
-            printf("%c ", scacchiera[i][j]);
+            if (islower(s.scacchiera[i][j]) && s.scacchiera[i][j] != 'O')
+                printf("\033[1;34m%c\033[0m ", s.scacchiera[i][j]);
+            else if (isupper(s.scacchiera[i][j]) && s.scacchiera[i][j] != 'O')
+                printf("\033[1;37m%c\033[0m ", s.scacchiera[i][j]);
+            else
+                printf("%c ", s.scacchiera[i][j]);
         }
         printf("\n");
     }
 }
-
-char **crea_scacchiera()
+// Ruoto la scacchiera dopo la mossa di uno dei due giocatori
+void ruota_scacchiera(Scacchiera *s)
 {
-    char **scacchiera;
+    for (int i = 0, len = 7; i < 4; i++, len--)
+    {
+        char *temp = s->scacchiera[i];
+        s->scacchiera[i] = s->scacchiera[len];
+        s->scacchiera[len] = temp;
+    }
+    s->ruotata = !(s->ruotata);
+}
+// Creo una scacchiera, utile farlo in una funzione per consentire al giocatore di giocare più di una partita per esecuzione
+// del programma
+Scacchiera crea_scacchiera()
+{
+    Scacchiera scacchiera;
+    scacchiera.ruotata = 0;
 
-    scacchiera = malloc(8 * sizeof(char *));
+    scacchiera.scacchiera = malloc(8 * sizeof(char *));
     for (int i = 0; i < 8; i++)
     {
-        *(scacchiera + i) = malloc(8 * sizeof(char));
+        *(scacchiera.scacchiera + i) = malloc(8 * sizeof(char));
     }
     for (int i = 0; i < 4; i++)
     {
@@ -35,21 +70,21 @@ char **crea_scacchiera()
                 {
                 case 0:
                 case 7:
-                    scacchiera[i][j] = 'r';
+                    scacchiera.scacchiera[i][j] = 'r';
                     break;
                 case 1:
                 case 6:
-                    scacchiera[i][j] = 'b';
+                    scacchiera.scacchiera[i][j] = 'b';
                     break;
                 case 2:
                 case 5:
-                    scacchiera[i][j] = 'n';
+                    scacchiera.scacchiera[i][j] = 'n';
                     break;
                 case 3:
-                    scacchiera[i][j] = 'q';
+                    scacchiera.scacchiera[i][j] = 'q';
                     break;
                 case 4:
-                    scacchiera[i][j] = 'k';
+                    scacchiera.scacchiera[i][j] = 'k';
                 }
             }
         }
@@ -58,7 +93,7 @@ char **crea_scacchiera()
             int indice = i == 1 ? i : 6;
             for (int j = 0; j < 8; j++)
             {
-                scacchiera[indice][j] = i == 1 ? 'i' : 'I';
+                scacchiera.scacchiera[indice][j] = i == 1 ? 'i' : 'I';
             }
         }
 
@@ -70,21 +105,21 @@ char **crea_scacchiera()
                 {
                 case 0:
                 case 7:
-                    scacchiera[7][j] = 'R';
+                    scacchiera.scacchiera[7][j] = 'R';
                     break;
                 case 1:
                 case 6:
-                    scacchiera[7][j] = 'B';
+                    scacchiera.scacchiera[7][j] = 'B';
                     break;
                 case 2:
                 case 5:
-                    scacchiera[7][j] = 'N';
+                    scacchiera.scacchiera[7][j] = 'N';
                     break;
                 case 3:
-                    scacchiera[7][j] = 'Q';
+                    scacchiera.scacchiera[7][j] = 'Q';
                     break;
                 case 4:
-                    scacchiera[7][j] = 'K';
+                    scacchiera.scacchiera[7][j] = 'K';
                 }
             }
         }
@@ -94,7 +129,7 @@ char **crea_scacchiera()
     {
         for (int j = 0; j < 8; j++)
         {
-            scacchiera[i][j] = 'O';
+            scacchiera.scacchiera[i][j] = 'O';
         }
     }
     return scacchiera;
@@ -102,7 +137,9 @@ char **crea_scacchiera()
 
 int main()
 {
-    char **scacchiera = crea_scacchiera();
+    Scacchiera scacchiera = crea_scacchiera();
+    stampa_scacchiera(scacchiera);
 
+    ruota_scacchiera(&scacchiera);
     stampa_scacchiera(scacchiera);
 }
